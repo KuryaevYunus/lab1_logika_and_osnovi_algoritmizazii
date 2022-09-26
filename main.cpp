d878f66d956dfa0bb6ab0fec268faf3bc5dcbd69
KuryaevYunus <112924139+KuryaevYunus@users.noreply.github.com>
2022-09-26 14:44:56 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,67 +2,48 @@
 #include <locale.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
-int main()
+struct student
 {
-    int** a;
-    int i, j, sum, sum2, N, M;
-    setlocale (LC_ALL,"RUS");
-    printf("Введите количество строк: ");
-    scanf("%d", &N);
-    printf("Введите количество столбцов: ");
-    scanf("%d", &M);
-    a = (int**)malloc(N * sizeof(int*));
-    srand(time(NULL));
-    for (i = 0; i < N; i++)
-    {
+    char family [20];
+    char facult[20];
+    char name [20];
+};
 
-        a[i] = (int*)malloc(M * sizeof(int));
-        for (j = 0; j < M; j++)
-        {
-            a[i][j] = rand() % 100;
-        }
+int main(void) {
+    int i;
+    int k = 0;
+    setlocale(LC_ALL, "Russian");
+    struct student stud [3];
+    struct student tmp;
+
+    for (int i = 0; i < 3; i++) {
+        printf("%d", i);
+        printf("\n Введите фамилию студента: ");
+        scanf("%s", stud[i].family);
+        printf(" Введите имя студента: ");
+        scanf("%s", stud[i].name);
+        printf(" Введите факультет студента: ");
+        scanf("%s", stud[i].facult);
     }
 
-    sum = 0;
-    for (i = 0; i < N; i++)
-    {
-        for (j = 0; j < M; j++)
-        {
-            printf("%d ", a[i][j]);
-        }
-        printf("\n");
-    }
-    printf("Суммы значений в каждой строке: ");
-    for (i = 0; i < N; i++)
-    {
-        sum = 0;
-        for (j = 0; j < M; j++)
-        {
-            sum = sum + a[i][j];
-        } printf("%d ", sum);
-    }
-    printf("\n");
-    printf("Суммы значений в каждом столбце: ");
+    char targetString[20];
+    printf ("\nВведите ключевое слово: ");
+    scanf("%s", targetString);
 
-    for (j = 0; j < M; j++)
-    {
-        sum = 0;
-        for (i = 0; i < N; i++)
-        {
-            sum = sum + a[i][j];
-        } printf("%d ", sum);
+    for (int i = 0; i < 3; i++) {
+        if (strcmp(targetString, stud[i].family) == 0) {
+            printf("Результат: Студент %s %s обучается на факультете %s\n", stud[i].family, stud[i].name, stud[i].facult);}
+            if (strcmp(targetString, stud[i].name) == 0) {
+                printf("Результат: Студент %s %s обучается на факультете %s\n", stud[i].family, stud[i].name, stud[i].facult);}
+            if (strcmp(targetString, stud[i].facult) == 0) {
+                printf("Результат: Студент %s %s обучается на факультете %s\n", stud[i].family, stud[i].name, stud[i].facult);}
+
+            k = 1;}
+
+        if (k == 0) {
+        printf("Не найдено");
     }
-
-
-    for (i = 0; i < N; i++)
-    {
-        for (j = i; j < M; j++) {
-
-                sum2 += a[i][j];
-        }
-    }
-    printf("\n");
-    printf("%d - Сумма элекментов, находящихся выше главной диагонали", sum2);
     return 0;
 }
